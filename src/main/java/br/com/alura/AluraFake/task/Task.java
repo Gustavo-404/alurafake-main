a73bf4a42cd34c60c6c8f339f4929e4389e0b3cf
Gustavo-404 <gustavogabriel77849@gmail.com>
2025-10-03 15:01:39 -0300

    feat: add task and option entities

---
+++ src/main/java/br/com/alura/AluraFake/task/Task.java
@@ -0,0 +1,54 @@
+package br.com.alura.AluraFake.task;
+
+import br.com.alura.AluraFake.course.Course;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Task")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "task_type", discriminatorType = DiscriminatorType.STRING)
+public abstract class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String statement;
+
+    @Column(name = "task_order", nullable = false)
+    private int order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Deprecated
+    public Task() {}
+
+    public Task(Course course, String statement, int order) {
+        this.course = course;
+        this.statement = statement;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+}
