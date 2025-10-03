1588f7937aba318186219e500df59b4b3652bc16
Gustavo-404 <gustavogabriel77849@gmail.com>
2025-10-03 17:18:01 -0300

    feat: integrating controller with service

--- src/main/java/br/com/alura/AluraFake/task/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskController.java
@@ -1,24 +1,36 @@
 package br.com.alura.AluraFake.task;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @PostMapping("/task/new/opentext")
-    public ResponseEntity newOpenTextExercise() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> newOpenTextTask(@RequestBody @Valid NewOpenTextTaskDTO dto) {
+        taskService.createTask(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/task/new/singlechoice")
-    public ResponseEntity newSingleChoice() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> newSingleChoiceTask(@RequestBody @Valid NewSingleChoiceTaskDTO dto) {
+        taskService.createTask(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/task/new/multiplechoice")
-    public ResponseEntity newMultipleChoice() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> newMultipleChoiceTask(@RequestBody @Valid NewMultipleChoiceTaskDTO dto) {
+        taskService.createTask(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
 }
